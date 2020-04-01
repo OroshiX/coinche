024a9ac5fc87673e22196fc69e445920ac91960c
istiti <yustina.hornik@gmail.com>
2020-04-01 10:42:48 +0200

    add msg error handler for login in Angular project

--- Web/src/app/login-page/login-page.component.ts
+++ Web/src/app/login-page/login-page.component.ts
@@ -1,24 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { LoginErrorStateMatcher, PWD_PATTERN } from '../utils/login-error-state-matcher';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
   loginForm: FormGroup;
+  matcher = new LoginErrorStateMatcher();
   hide = true;
+  sub: Subscription;
 
-  constructor(private formBuilder: FormBuilder) {
+  get userId(): FormControl {
+    return this.loginForm.get('userId') as FormControl;
+  }
+
+  get currentPassword(): FormControl {
+    return this.loginForm.get('currentPassword') as FormControl;
+  }
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {
   }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-        userId: this.formBuilder.control('', Validators.required),
-        password: this.formBuilder.control('', Validators.required)
+        userId: this.formBuilder.control('',
+          [Validators.required, Validators.minLength(6)]),
+        currentPassword: this.formBuilder.control('',
+          [Validators.required, Validators.pattern(PWD_PATTERN)])
       }
     );
+
+    this.sub = this.loginForm.valueChanges.subscribe(value => console.log(value));
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  submit() {
+    if (this.loginForm.valid) {
+      this.router.navigateByUrl('play').then(res => console.log(res));
+      console.log('navigate');
+    }
   }
 
 }
