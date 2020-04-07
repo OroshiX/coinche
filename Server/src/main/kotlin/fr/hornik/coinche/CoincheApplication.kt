e2842e0ec9a71eef4883577725350b0fc2eb51ef
Armand H <armand@dev.hornik.fr>
2020-04-07 19:18:30 +0200

    add some debug traces for the server to get more info for debuging application properties should remove debug flag once in application working correctly

--- Server/src/main/kotlin/fr/hornik/coinche/CoincheApplication.kt
+++ Server/src/main/kotlin/fr/hornik/coinche/CoincheApplication.kt
@@ -2,10 +2,28 @@ package fr.hornik.coinche
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.filter.CommonsRequestLoggingFilter
 
 @SpringBootApplication
 class CoincheApplication
 
 fun main(args: Array<String>) {
     runApplication<CoincheApplication>(*args)
+}
+
+@Configuration
+class RequestLoggingFilterConfig {
+
+    @Bean
+    fun logFilter(): CommonsRequestLoggingFilter {
+        val filter = CommonsRequestLoggingFilter()
+        filter.setIncludeQueryString(true)
+        filter.setIncludePayload(true)
+        filter.setMaxPayloadLength(10000)
+        filter.setIncludeHeaders(true)
+        filter.setAfterMessagePrefix("REQUEST DATA : ")
+        return filter
+    }
 }
