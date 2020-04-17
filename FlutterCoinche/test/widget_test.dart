5be7ef183f25e581c71092723e00b3fd7042b06f
OroshiX <oroshix@users.noreply.github.com>
2020-04-17 14:35:16 +0200

    A working test, and workflow with dart tool

--- FlutterCoinche/test/widget_test.dart
+++ FlutterCoinche/test/widget_test.dart
@@ -5,25 +5,43 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:FlutterCoinche/main.dart';
+import 'package:FlutterCoinche/dto/card.dart' as cardModel;
+import 'package:FlutterCoinche/widget/card_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: Container(
+          width: 50,
+          height: 60,
+          child: CardWidget(
+            card: cardModel.Card(
+                color: cardModel.CardColor.CLUB,
+                playable: true,
+                value: cardModel.CardValue.KING),
+            width: 40,
+            height: 60,
+            displayPlayable: true,
+          ),
+        ),
+      ),
+    ));
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+    expect(find.text('K'), findsNWidgets(2));
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+//    await tester.tap(find.byIcon(Icons.add));
+//    await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+//    expect(find.text('0'), findsNothing);
+//    expect(find.text('1'), findsOneWidget);
   });
 }
