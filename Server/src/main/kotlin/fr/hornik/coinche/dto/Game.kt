a0d7072f741b9182e7413ad2f47e35113eaafbaf
Armand H <armand@dev.hornik.fr>
2020-05-01 00:22:15 +0200

    add status in allGames REST operation return

--- Server/src/main/kotlin/fr/hornik/coinche/dto/Game.kt
+++ Server/src/main/kotlin/fr/hornik/coinche/dto/Game.kt
@@ -5,10 +5,11 @@ import fr.hornik.coinche.model.SetOfGames
 data class Game(val id: String,
                 val nbJoined: Int,
                 val name: String,
-                val nicknameCreator: String, val inRoom: Boolean) {
+                val nicknameCreator: String, val state:String, val inRoom: Boolean) {
     constructor(set: SetOfGames, inRoom: Boolean) : this(set.id,
             set.players.size,
             set.name,
             set.players.first().nickname,
+            set.state.value,
             inRoom);
 }
