33af58f3f70b3ea2cfb14cfccdf228358afa9b48
Armand H <armand@dev.hornik.fr>
2020-05-03 20:16:14 +0200

    Fix bugs in playcards and associated functions add a Makefile on top[ of the workspace

--- Server/src/test/kotlin/fr/hornik/coinche/CalculusTest.kt
+++ Server/src/test/kotlin/fr/hornik/coinche/CalculusTest.kt
@@ -14,28 +14,35 @@ import org.junit.jupiter.api.Test
 
 class CalculusTest {
     private lateinit var bids: List<Bid>
- 
-
+    private val south = PlayerPosition.SOUTH
+    private val north = PlayerPosition.NORTH
+    private val west = PlayerPosition.WEST
+    private val east = PlayerPosition.EAST
+    private val heart = CardColor.HEART
+    private val club = CardColor.CLUB
+    private val spade = CardColor.SPADE
+    private val diamond = CardColor.DIAMOND
 
 
     @BeforeEach
     fun initBids() {
         //traceLevel = dbgLevel.NONE
 
-        debugPrintln(dbgLevel.FUNCTION,"Enter initBids")
+        debugPrintln(dbgLevel.FUNCTION, "Enter initBids")
 
         bids = listOf(
-                SimpleBid(CardColor.HEART, 80, PlayerPosition.NORTH),
-                Pass(PlayerPosition.EAST),
-                Capot(CardColor.SPADE, PlayerPosition.NORTH, true),
-                General(CardColor.DIAMOND, PlayerPosition.SOUTH, false),
-                Coinche(SimpleBid(CardColor.CLUB, 90, PlayerPosition.WEST), PlayerPosition.NORTH, surcoinche = true))
+                SimpleBid(heart, 80, north),
+                Pass(east),
+                Capot(spade, north, true),
+                General(diamond, south, false),
+                Coinche(SimpleBid(club, 90, west), north, surcoinche = true))
     }
 
     @Test
     fun testBid() {
         //testUnit()
-        debugPrintln(dbgLevel.FUNCTION,"Enter testBid")
+        val nameTest = object {}.javaClass.enclosingMethod.name
+
 
         var EW: MutableList<MutableList<CardPlayed>> = mutableListOf()
         var NS: MutableList<MutableList<CardPlayed>> = mutableListOf()
@@ -48,6 +55,7 @@ class CalculusTest {
             NS = aPair.second
 
         }
+        assert(true) { "$nameTest FAIL " }
 
     }
 
@@ -65,6 +73,7 @@ class CalculusTest {
         // EAST : 100 HEART
         // East plays 10 Heart
         // South should be able to play K HEART  and only K Heart
+        val nameTest = object {}.javaClass.enclosingMethod.name
 
         var myCards: MutableList<Card> = mutableListOf<Card>()
         var currBid :Bid = SimpleBid(CardColor.HEART, 100, PlayerPosition.EAST)
@@ -89,11 +98,9 @@ class CalculusTest {
         for (mcard in myCards) {
             mcard.playable = c.contains(mcard)
         }
-        if ((myCards.filter{e -> (e.playable == true)}.size != 1 ) || (myCards.filter{e -> (e.playable == true)}.first().color != CardColor.HEART)) {
-            println("********FAIL - Activate trace for details (CalculusTest.TraceLevel to DEBUG) ***************\n")
-        } else {
-            println("TEST testValidCartes PASS\n")
-        }
+        assert(!((myCards.filter{e -> (e.playable == true)}.size != 1 ) || (myCards.filter{e -> (e.playable == true)}.first().color != CardColor.HEART))) { "$nameTest FAIL " }
+        debugPrintln(dbgLevel.REGULAR, "$nameTest:PASS .......")
+
 
 
     }
@@ -107,6 +114,9 @@ class CalculusTest {
         // West has AS 10S AH 9H 7H JC JD 7D
         // playable should be true for A,9 or 7 H
 
+        val nameTest = object {}.javaClass.enclosingMethod.name
+        val oldTraceLevel = traceLevel
+        //traceLevel = dbgLevel.ALL
 
         var myCards: MutableList<Card> = mutableListOf<Card>()
         var currBid :Bid = SimpleBid(CardColor.SPADE, 100, PlayerPosition.NORTH)
@@ -139,18 +149,16 @@ class CalculusTest {
 
 
 
-        if ((c.size != 3 ) || c.any{ e -> (e.color != CardColor.HEART)} ){
-                    println("********FAIL - Activate trace for details (CalculusTest.TraceLevel to DEBUG) ***************\n")
-                } else {
-            println("TEST testCoincheValidCartes PASS\n")
-        }
-
-
+        traceLevel = oldTraceLevel
+        assert((c.size == 3 ) || c.none { e -> (e.color != CardColor.HEART)} ) { "$nameTest FAIL " }
+        debugPrintln(dbgLevel.REGULAR, "$nameTest:PASS .......")
     }
 
 
     fun testPartie(pliNS: MutableList<MutableList<CardPlayed>>, pliEW: MutableList<MutableList<CardPlayed>>): Pair<MutableList<MutableList<CardPlayed>>, MutableList<MutableList<CardPlayed>>> {
-        debugPrintln(dbgLevel.FUNCTION,"Enter testPartie")
+        val nameTest = object {}.javaClass.enclosingMethod.name
+        val oldTraceLevel = traceLevel
+        //traceLevel = dbgLevel.ALL
 
         debugPrintln(dbgLevel.HTML, "<table><tr><td>\n")
         val mapPos = mapOf(PlayerPosition.NORTH to 0,
@@ -226,6 +234,12 @@ class CalculusTest {
             printHand(dbgLevel.DEBUG, li.map { it -> it.card }, "North South Pli $i")
         }
         debugPrintln(dbgLevel.HTML, "</pre><br><br><br>")
+
+        traceLevel = oldTraceLevel
+
+        // Not really a unit test more a system test .... Ananlysis is manual.
+        assert(true) { "$nameTest FAIL " }
+        debugPrintln(dbgLevel.REGULAR, "$nameTest:PASS .......")
         return (Pair(PlisEW, PlisNS))
 
     }
@@ -235,74 +249,400 @@ class CalculusTest {
     fun Bid001() {
         val nameTest = object {}.javaClass.enclosingMethod.name
 
+
+        // Scenario :
+        // N : PASS
+        // E : 80 Heart
+        // S : 100 DIAMOND
+        // W : 100 HEART -> should be Invalid
+
+
         val oldTraceLevel = traceLevel
         //traceLevel = dbgLevel.ALL
+        val allBids = listOf(
+                Pass(north),
+                SimpleBid(heart, 80, east),
+                SimpleBid(diamond, 100, south)
+        )
+        val bid = SimpleBid(heart, 100, west)
 
-        val prevBid = SimpleBid(CardColor.DIAMOND, 160, PlayerPosition.SOUTH)
-        val listBids = listOf(Pass(), prevBid, Pass())
+        val Returnvalue = isValidBid(allBids, bid)
 
-
-        assert(true) { "$nameTest FAIL - ...." }
+        assert(!Returnvalue) { "$nameTest FAIL " }
         debugPrintln(dbgLevel.REGULAR, "$nameTest:PASS .......")
     }
 
     @Test
     fun Bid002() {
+
+
+        // Scenario :
+        // N : PASS
+        // E : 80 Heart
+        // S : 100 DIAMOND
+        // W : 110 HEART -> should be Valid
+
         val nameTest = object {}.javaClass.enclosingMethod.name
 
         val oldTraceLevel = traceLevel
         //traceLevel = dbgLevel.ALL
 
-        val prevBid = SimpleBid(CardColor.DIAMOND, 160, PlayerPosition.SOUTH)
-        val listBids = listOf(Pass(), prevBid, Pass())
+        val allBids = listOf(
+                Pass(north),
+                SimpleBid(heart, 80, east),
+                SimpleBid(diamond, 100, south)
+        )
+        val bid = SimpleBid(heart, 110, west)
 
+        val Returnvalue = isValidBid(allBids, bid)
 
-        assert(true) { "$nameTest FAIL - ...." }
+        assert(Returnvalue) { "$nameTest FAIL " }
         debugPrintln(dbgLevel.REGULAR, "$nameTest:PASS .......")
 
     }
     @Test
     fun Bid003() {
+
+        // Scenario :
+        // N : PASS
+        // E : 80 Heart
+        // S : 100 DIAMOND
+        // W : PASS
+        // N : Coinche --> should be invalid
+
+
         val nameTest = object {}.javaClass.enclosingMethod.name
 
         val oldTraceLevel = traceLevel
         //traceLevel = dbgLevel.ALL
 
-        val prevBid = SimpleBid(CardColor.DIAMOND, 160, PlayerPosition.SOUTH)
-        val listBids = listOf(Pass(), prevBid, Pass())
+        val allBids = listOf(
+                Pass(north),
+                SimpleBid(heart, 80, east),
+                SimpleBid(diamond, 100, south),
+                Pass(west)
+        )
+        val bid = Coinche(SimpleBid(), north, surcoinche = false)
+        val Returnvalue = isValidBid(allBids, bid)
 
-
-        assert(true) { "$nameTest FAIL - ...." }
+        assert(!Returnvalue) { "$nameTest FAIL " }
         debugPrintln(dbgLevel.REGULAR, "$nameTest:PASS .......")
 
     }
     @Test
     fun Bid004() {
+        // Scenario :
+        // N : PASS
+        // E : 80 Heart
+        // S : 100 DIAMOND
+        // W : PASS
+        // N : PASS
+        // E : Coinche --> should be valid
+
         val nameTest = object {}.javaClass.enclosingMethod.name
 
         val oldTraceLevel = traceLevel
         //traceLevel = dbgLevel.ALL
 
-        val prevBid = SimpleBid(CardColor.DIAMOND, 160, PlayerPosition.SOUTH)
-        val listBids = listOf(Pass(), prevBid, Pass())
+        val allBids = listOf(
+                Pass(north),
+                SimpleBid(heart, 80, east),
+                SimpleBid(diamond, 100, south),
+                Pass(west),
+                Pass(north)
+        )
+        val bid = Coinche(SimpleBid(), east, surcoinche = false)
+        val Returnvalue = isValidBid(allBids, bid)
 
-
-        assert(true) { "$nameTest FAIL - ...." }
+        assert(Returnvalue) { "$nameTest FAIL " }
         debugPrintln(dbgLevel.REGULAR, "$nameTest:PASS .......")
 
     }
     @Test
     fun Bid005() {
+        // Scenario :
+        // N : PASS
+        // E : 80 Heart
+        // S : 100 DIAMOND
+        // W : PASS
+        // N : PASS
+        // E : COINCHE
+        // S : PASS
+        // W : PASS
+        // N : SURCOINCHE -> Valid
+
         val nameTest = object {}.javaClass.enclosingMethod.name
 
         val oldTraceLevel = traceLevel
         //traceLevel = dbgLevel.ALL
 
-        val prevBid = SimpleBid(CardColor.DIAMOND, 160, PlayerPosition.SOUTH)
-        val listBids = listOf(Pass(), prevBid, Pass())
+        val allBids = listOf(
+                Pass(north),
+                SimpleBid(heart, 80, east),
+                SimpleBid(diamond, 100, south),
+                Pass(west),
+                Pass(north),
+                Coinche(SimpleBid(), east, false),
+                Pass(south),
+                Pass(west)
+        )
+        val bid = Coinche(SimpleBid(), east, surcoinche = true)
+        val Returnvalue = isValidBid(allBids, bid)
+
+        assert(Returnvalue) { "$nameTest FAIL " }
+        debugPrintln(dbgLevel.REGULAR, "$nameTest:PASS .......")
+
+    }
+
+    @Test
+    fun Bid006() {
+        // Scenario :
+        // N : PASS
+        // E : 80 Heart
+        // S : 100 DIAMOND
+        // W : PASS
+        // N : PASS
+        // E : COINCHE
+        // S : PASS
+        // W : SURCOINCHE -> Invalid
+
+        val nameTest = object {}.javaClass.enclosingMethod.name
+
+        val oldTraceLevel = traceLevel
+        //traceLevel = dbgLevel.ALL
+
+        val allBids = listOf(
+                Pass(north),
+                SimpleBid(heart, 80, east),
+                SimpleBid(diamond, 100, south),
+                Pass(west),
+                Pass(north),
+                Coinche(SimpleBid(), east, false),
+                Pass(south)
+        )
+        val bid = Coinche(SimpleBid(), west, surcoinche = true)
+        val Returnvalue = isValidBid(allBids, bid)
+
+        assert(!Returnvalue) { "$nameTest FAIL " }
+        debugPrintln(dbgLevel.REGULAR, "$nameTest:PASS .......")
+
+    }
+
+    @Test
+    fun Bid007() {
+        // Scenario :
+        // E : PASS
+        // S : 80 Heart
+        // W : 100 DIAMOND
+        // N : PASS
+        // E : PASS
+        // S : COINCHE
+        // W : PASS
+        // N : SURCOINCHE -> Invalid
+        val nameTest = object {}.javaClass.enclosingMethod.name
+
+        val oldTraceLevel = traceLevel
+        //traceLevel = dbgLevel.ALL
+
+        val allBids = listOf(
+                Pass(east),
+                SimpleBid(heart, 80, south),
+                SimpleBid(diamond, 100, west),
+                Pass(north),
+                Pass(east),
+                Coinche(SimpleBid(), south, false),
+                Pass(west)
+        )
+        val bid = Coinche(SimpleBid(), north, surcoinche = true)
+        val Returnvalue = isValidBid(allBids, bid)
+
+        assert(!Returnvalue) { "$nameTest FAIL " }
+        debugPrintln(dbgLevel.REGULAR, "$nameTest:PASS .......")
+
+    }
+
+    @Test
+    fun Bid008() {
+        // Scenario :
+        // S : PASS
+        // W : 80 Heart
+        // N : 100 DIAMOND
+        // E : PASS
+        // S : PASS
+        // W : COINCHE
+        // N : PASS
+        // E : SURCOINCHE -> Invalid
 
 
-        assert(true) { "$nameTest FAIL - ...." }
+        val nameTest = object {}.javaClass.enclosingMethod.name
+
+        val oldTraceLevel = traceLevel
+        //traceLevel = dbgLevel.ALL
+
+        val allBids = listOf(
+                Pass(south),
+                SimpleBid(heart, 80, west),
+                SimpleBid(diamond, 100, north),
+                Pass(east),
+                Pass(south),
+                Coinche(SimpleBid(), west, false),
+                Pass(north)
+        )
+        val bid = Coinche(SimpleBid(), east, surcoinche = true)
+        val Returnvalue = isValidBid(allBids, bid)
+
+        assert(!Returnvalue) { "$nameTest FAIL " }
+        debugPrintln(dbgLevel.REGULAR, "$nameTest:PASS .......")
+
+    }
+
+    @Test
+    fun Bid009() {
+        // Scenario :
+        // N : 80 Heart
+        // E : 100 DIAMOND
+        // S : PASS
+        // W : PASS
+        // N : COINCHE
+        // E : PASS
+        // S : SURCOINCHE -> Invalid
+
+        val nameTest = object {}.javaClass.enclosingMethod.name
+
+        val oldTraceLevel = traceLevel
+        //traceLevel = dbgLevel.ALL
+
+        val allBids = listOf(
+                Pass(west),
+                SimpleBid(heart, 80, north),
+                SimpleBid(diamond, 100, east),
+                Pass(south),
+                Pass(west),
+                Coinche(SimpleBid(), north, false),
+                Pass(east)
+        )
+        val bid = Coinche(SimpleBid(), south, surcoinche = true)
+        val Returnvalue = isValidBid(allBids, bid)
+
+        assert(!Returnvalue) { "$nameTest FAIL " }
+        debugPrintln(dbgLevel.REGULAR, "$nameTest:PASS .......")
+
+    }
+
+    @Test
+    fun Bid010() {
+        // Scenario :
+        // E : PASS
+        // S : 90 Heart
+        // W : 100 DIAMOND
+        // N : PASS
+        // E : PASS
+        // S : COINCHE
+        // W : PASS
+        // N : PASS
+        // E : SURCOINCHE -> Valid
+
+        val nameTest = object {}.javaClass.enclosingMethod.name
+
+        val oldTraceLevel = traceLevel
+        //traceLevel = dbgLevel.ALL
+
+        val allBids = listOf(
+                Pass(east),
+                SimpleBid(heart, 90, south),
+                SimpleBid(diamond, 100, west),
+                Pass(north),
+                Pass(east),
+                Coinche(SimpleBid(), south, false),
+                Pass(west),
+                Pass(north)
+        )
+        val bid = Coinche(SimpleBid(), east, surcoinche = true)
+        val Returnvalue = isValidBid(allBids, bid)
+
+        assert(Returnvalue) { "$nameTest FAIL " }
+        debugPrintln(dbgLevel.REGULAR, "$nameTest:PASS .......")
+
+    }
+
+    @Test
+    fun Bid011() {
+        // Scenario :
+        // S : 160 Diamond
+        // W : 180 Heart
+        // N : PASS
+        // E : PASS
+        // S : CAPOT Heart
+        // W : CAPOT BELOTE DIAMOND
+        // N : PASS
+        // E : PASS
+        // S : GENERALE HEART
+        // W : PASS
+        // N : PASS
+        // E : GENERALE BELOTEE
+        // S : Coinche
+        // W : PASS
+        // N : PASS
+        // W : Surcoinche
+        // All Bids are Valide
+
+        val nameTest = object {}.javaClass.enclosingMethod.name
+
+        val oldTraceLevel = traceLevel
+        //traceLevel = dbgLevel.ALL
+
+        val allBids = listOf(
+                SimpleBid(diamond, 160, south),
+                SimpleBid(heart, 180, west),
+                Pass(north),
+                Pass(east),
+                Capot(heart, south),
+                Capot(diamond, west, true),
+                Pass(north),
+                Pass(east),
+                General(diamond, south),
+                Pass(west),
+                Pass(north),
+                General(heart, east, true),
+                Coinche(SimpleBid(), south),
+                Pass(west),
+                Pass(north),
+                Coinche(SimpleBid(), west, true)
+        )
+
+        val callBids : MutableList<Bid> = mutableListOf()
+
+        for (bid in allBids) {
+            debugPrintln(dbgLevel.DEBUG,"$nameTest: callBids = $callBids and we bet : $bid")
+            assert(isValidBid(callBids, bid)) { "$nameTest FAIL " }
+            callBids.add(bid)
+        }
+
+        debugPrintln(dbgLevel.REGULAR, "$nameTest:PASS .......")
+
+    }
+
+    @Test
+    fun Bid012() {
+        // Scenario :
+        // Template for more Bid tests if needed
+
+        val nameTest = object {}.javaClass.enclosingMethod.name
+
+        val oldTraceLevel = traceLevel
+        //traceLevel = dbgLevel.ALL
+
+
+        val allBids : List<Bid> = listOf( )
+
+        val callBids : MutableList<Bid> = mutableListOf()
+
+        for (bid in allBids) {
+            debugPrintln(dbgLevel.DEBUG,"$nameTest: callBids = $callBids and we bet : $bid")
+            assert(isValidBid(callBids, bid)) { "$nameTest FAIL " }
+            callBids.add(bid)
+        }
+
         debugPrintln(dbgLevel.REGULAR, "$nameTest:PASS .......")
 
     }
