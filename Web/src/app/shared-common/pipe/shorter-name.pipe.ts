bbf4b631f887bb46226b35707fbbbdf3e6e32afd
istiti <yustina.hornik@gmail.com>
2020-05-05 23:59:31 +0200

    Not to show SimpleBid, correction on pipe shortened in Angular project

--- Web/src/app/shared-common/pipe/shorter-name.pipe.ts
+++ Web/src/app/shared-common/pipe/shorter-name.pipe.ts
@@ -6,13 +6,26 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ShorterNamePipe implements PipeTransform {
 
   transform(value: string, limit: number): string {
-    if (!!value && value.length > 0) {
-      const arr = value.split('-');
-      return value.length <= limit ? value : arr.length > 0 ?
-        arr[0].charAt(0) + '-' + arr[1].substr(0, limit - 1) :
-        value.substr(0, limit);
-    } else {
+    if (!!value && value.length <= limit) {
       return value;
+    } else if (!!value && value.length > limit) {
+      const arrHyphen = value.split('-');
+      const arrSpace = value.split(' ');
+      const arrUnderS = value.split('_');
+      if (arrHyphen.length > 1) {
+        const valShortened = arrHyphen[0].charAt(0) + '-' + arrHyphen[1];
+        return valShortened.slice(0, limit);
+      } else if (arrSpace.length > 1) {
+        const valS = arrSpace[0].trim().charAt(0) + ' ' + arrSpace[1].trim();
+        return valS.slice(0, limit);
+      } else if (arrUnderS.length > 1) {
+        const valS = arrUnderS[0].trim().charAt(0) + '_' + arrUnderS[1].trim();
+        return valS.slice(0, limit);
+      } else {
+        return value.slice(0, limit);
+      }
+    } else {
+      return '';
     }
   }
 
