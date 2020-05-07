9123e847235c13d750f403108ec612cdc0fa5e7f
OroshiX <oroshix@users.noreply.github.com>
2020-05-07 13:55:06 +0200

    [Flutter] First working hierarchy states_builder

--- FlutterCoinche/lib/ui/screen/stated_game_screen.dart
+++ FlutterCoinche/lib/ui/screen/stated_game_screen.dart
@@ -1,10 +1,12 @@
-import 'package:FlutterCoinche/state/games_bloc.dart';
 import 'package:FlutterCoinche/domain/dto/bid.dart';
 import 'package:FlutterCoinche/domain/dto/card.dart';
 import 'package:FlutterCoinche/domain/dto/game.dart';
 import 'package:FlutterCoinche/domain/dto/player_position.dart';
 import 'package:FlutterCoinche/state/game_inherited.dart';
+import 'package:FlutterCoinche/state/games_bloc.dart';
 import 'package:bloc_provider/bloc_provider.dart';
+import 'package:flushbar/flushbar_helper.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:states_rebuilder/states_rebuilder.dart';
@@ -19,67 +21,87 @@ class StatedGameScreen extends StatelessWidget {
         onWillPop: () async {
           return (await _quit(context)) ?? false;
         },
-        child: Injector(
-          inject: [
-            Inject.stream(() => BlocProvider.of<GamesBloc>(context).game,
-                name: "game"),
-            Inject(() => Game()),
-            Inject.future(() => getPosTableToColors())
-          ],
-          initState: () {
-            // To be executed in the initState of StatefulWidget
-          },
-          builder: (BuildContext context) {
-            final ReactiveModel<Game> gameFire =
-                Injector.getAsReactive<Game>(name: "game");
-            final ReactiveModel<Game> game = Injector.getAsReactive<Game>();
-
-            return StateBuilder(
-              models: [gameFire],
-              onSetState: (_, model) {
-                game.setValue(() => gameFire.value);
-              },
-              builder: (_, __) {
-                return Scaffold(
-                    appBar: AppBar(
-                      title: Text("${game.value.id}"),
-                      actions: [
-                        FlatButton(
-                            onPressed: () {
-                              game.setState(
-                                (game) => game.bids.add(SimpleBid(
-                                    points: 90,
-                                    color: CardColor.HEART,
-                                    position: PlayerPosition.EAST)),
-                                filterTags: [Aspects.BIDS],
-                                catchError: true,
-                                onSetState: (context) {
-                                  if (game.hasError) {
-                                    showDialog(
-                                      context: context,
-                                      builder: (context) => AlertDialog(
-                                        title: Icon(Icons.error),
-                                        content: Text("${game.error}"),
-                                      ),
-                                    );
-                                  }
-                                },
-                              );
-                            },
-                            child: Text("add a bid"))
-                      ],
-                    ),
-                    floatingActionButton: FloatingActionButton(
-                        child: Icon(Icons.sort_by_alpha),
-                        onPressed: () =>
-                            game.setState((Game g) => g.sortCards())),
-                    body: TestWidget());
-              },
-            );
-          },
-        ),
+        child: _top(context),
       ),
     );
+  }
+
+  Widget _top(BuildContext context) {
+    return StateBuilder<Game>(
+      observe: () =>
+          RM.stream(BlocProvider.of<GamesBloc>(context).game, name: "gameFire"),
+      builder: (context, model) => WhenRebuilder<Game>(
+          models: [model],
+          onIdle: () => _buildEmpty(),
+          onWaiting: () => _buildWaiting(),
+          onError: (e) => _buildEmpty(),
+          onData: (gameFire) {
+            var model = Inject(() => gameFire);
+            return Injector(
+                inject: [model],
+                builder: (BuildContext buildContext) {
+                  return _build(model.getReactive());
+                });
+          }),
+    );
+    /*
+    return Injector(
+      inject: [
+        Inject.stream(() => BlocProvider.of<GamesBloc>(context).game,
+            name: "game")
+      ],
+      builder: (BuildContext buildContext) {
+        return WhenRebuilder<Game>(
+            observe: () => RM.get<Game>(name: "game"),
+            onIdle: () => _buildEmpty(),
+            onWaiting: () => _buildWaiting(),
+            onError: (e) => _buildEmpty(),
+            onData: (gameFire) {
+//              var reactiveModel = RM.create(gameFire).asNew();
+              return StateBuilder<Game>(
+                  onSetState: (context, model) {
+                    model.setValue(() => gameFire);
+                  },
+                  observe: () => RM.create(gameFire),
+//                  observeMany: [
+//                    () => reactiveModel,
+//                    () => RM.future(getPosTableToColors()),
+//                  ],
+                  builder:
+                      (BuildContext context, ReactiveModel<Game> gameModel) {
+                    if (gameModel.state != null) return _build(gameModel);
+                    return _buildEmpty();
+                  });
+            });
+      },
+    );
+    */
+  }
+
+  Widget _build(ReactiveModel<Game> gameModel) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("${gameModel.state.id}"),
+          actions: [
+            FlatButton(
+                onPressed: () {
+                  gameModel.setState(
+                    (g) => g.bids.add(SimpleBid(
+                        points: 90,
+                        color: CardColor.HEART,
+                        position: PlayerPosition.EAST)),
+                    filterTags: [Aspects.BIDS],
+                    catchError: true,
+                    onSetState: (context) {},
+                  );
+                },
+                child: Text("add a bid"))
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.sort_by_alpha),
+            onPressed: () => gameModel.setState((Game g) => g.sortCards())),
+        body: TestWidget());
   }
 
   Future _quit(BuildContext context) {
@@ -101,16 +123,31 @@ class StatedGameScreen extends StatelessWidget {
       ),
     );
   }
+
+  Widget _buildWaiting() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget _buildEmpty() {
+    return Center(
+      child: Text("Empty"),
+    );
+  }
 }
 
 class TestWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final ReactiveModel<Game> game =
-        Injector.getAsReactive<Game>(context: context);
+    final ReactiveModel<Game> game = RM.get<Game>();
     return StateBuilder(
       models: [game],
       tag: Aspects.BIDS,
+      onSetState: (context, model) {
+        FlushbarHelper.createInformation(message: "Hi, new Bids!")
+            .show(context);
+      },
       builder: (context, model) => Padding(
         padding: const EdgeInsets.all(20.0),
         child: Center(child: Text("${game.value}")),
